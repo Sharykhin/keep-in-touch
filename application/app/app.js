a4319aa64276ba99b4deb87efbbf5a0d416da29e
Siarhei Sharykhin <siarhei.sharykhin@gmail.com>
2015-04-22 08:55:55 +0300

    ISSUE-#1 has been completed

--- application/app/app.js
+++ application/app/app.js
@@ -3,21 +3,18 @@ define([
     './config',
     './src/frontend/module.config',
     './src/common/module.config',
-    './src/backend/module.config',
     'angular-route',
     'angular-cookies',
     './src/frontend/module.require',
-    './src/common/module.require',
-    './src/backend/module.require'
-],function(angular,config,frontendModuleConfig,commonModuleConfig,backendModuleConfig){
+    './src/common/module.require'   
+],function(angular,config,frontendModuleConfig,commonModuleConfig){
    'use strict';
 
     var app = angular.module(config.name,[
         'ngRoute',
         'ngCookies',
         frontendModuleConfig.name,
-        commonModuleConfig.name,
-        backendModuleConfig.name
+        commonModuleConfig.name        
     ]).run(function(){
 
     });
