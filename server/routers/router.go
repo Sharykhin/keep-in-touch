41cdd7146346b20c4798f0ad08a70a345ca2345e
Siarhei Sharykhin <siarhei.sharykhin@gmail.com>
2015-04-22 19:54:18 +0300

    Add filter for cross-domain-header

--- server/routers/router.go
+++ server/routers/router.go
@@ -2,9 +2,14 @@ package routers
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 	"keep-in-touch/server/controllers"
 )
 
 func init() {
+
+	beego.InsertFilter("/", beego.BeforeRouter, func(ctx *context.Context) {
+		ctx.Output.Header("Access-Control-Allow-Origin", "*")
+	})
 	beego.Router("/", &controllers.EchoController{})
 }
