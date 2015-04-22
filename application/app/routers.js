7fda8a087a78a71bfa8c5f603468e27a74b9315a
Siarhei Sharykhin <siarhei.sharykhin@gmail.com>
2015-04-22 20:03:04 +0300

    ISSUE-#5 has been completed. Angular-ui-router has been installed

--- application/app/routers.js
+++ application/app/routers.js
@@ -5,11 +5,24 @@ define([
 ],function(angular,app,frontendConfig){
     'use strict';
 
-    return app.config(function($routeProvider){
-        $routeProvider.when('/',{
-           templateUrl: 'src/' + frontendConfig.templatePath + 'index.html',
-           controller: frontendConfig.name + '.HomeController'
-        });
+    return app.config(function($stateProvider, $urlRouterProvider){
+
+      $urlRouterProvider.otherwise("/");
+     
+      // Now set up the states
+      $stateProvider
+        .state('home', {
+          url: "/",
+          templateUrl: 'src/' + frontendConfig.templatePath + 'index.html',
+          controller: frontendConfig.name + '.HomeController'
+        })
+        .state('home.index', {
+          url: "/index",
+          templateUrl: "partials/state1.list.html",
+          controller: function($scope) {
+            $scope.items = ["A", "List", "Of", "Items"];
+          }
+        });       
        
     });
 });
