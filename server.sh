8552b8de2c618b36fd65c7eb19e8ac5a48c09f0f
Siarhei Sharykhin <siarhei.sharykhin@gmail.com>
2015-04-27 08:25:15 +0300

    ISSUE-#16 has been completed

---
+++ server.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+
+start() {
+	cd application
+	gulp serve &
+	cd ../server
+	bee run &	
+	
+}
+
+stop() {
+	echo "Stopping server..."
+	kill -9 `ps -ef | grep "gulp-live-server" | grep -v "grep" | awk '{print $2}'`
+	kill -9 `ps -ef | grep "./server" | grep -v "grep" | awk '{print $2}'`
+	echo "Server has been stopped"	
+}
+
+restart() {	
+	stop
+	start		
+}
+
+case "$1" in
+start)
+	start
+	;;
+stop)
+	stop
+	;;
+restart)
+	restart
+	;;
+*)
+ 
+echo "Usage: {start|stop|restart}"
+	exit 1
+esac
