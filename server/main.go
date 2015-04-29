f31cca041ac9711623178aef6ad62f4c3d630d0e
Siarhei Sharykhin <siarhei.sharykhin@gmail.com>
2015-04-29 08:05:36 +0300

    ISSUE-#20 has been ccompleted

--- server/main.go
+++ server/main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
-	_ "keep-in-touch/server/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+	_ "keep-in-touch/server/modules/users/models"
+	_ "keep-in-touch/server/routers"
 )
 
-func main() {
-	beego.Run()
+var dbname string = "keep_in_touch"
+
+func init() {
+
+	orm.RegisterDriver("mysql", orm.DR_MySQL)
+
+	orm.RegisterDataBase("default", "mysql", "root:pass4root@/"+dbname+"?charset=utf8")
 }
 
+func main() {
+
+	name := "default"
+	// Drop table and re-create.
+	force := false
+
+	// Print log.
+	verbose := false
+
+	// Error.
+	err := orm.RunSyncdb(name, force, verbose)
+	if err != nil {
+		beego.Debug(err)
+	}
+
+	beego.Run()
+}
