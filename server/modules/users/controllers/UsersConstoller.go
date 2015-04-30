27886a109be9e30e33abf7c6123480df959f7e3d
Siarhei Sharykhin <siarhei.sharykhin@gmail.com>
2015-04-30 09:01:18 +0300

    ISSUE-#12. Working...

--- server/modules/users/controllers/UsersConstoller.go
+++ server/modules/users/controllers/UsersConstoller.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"encoding/json"
 	"github.com/astaxie/beego"
+	//"github.com/astaxie/beego/orm"
+	userModel "keep-in-touch/server/modules/users/models"
 )
 
 type UsersController struct {
@@ -13,4 +16,23 @@ func (this *UsersController) Get() {
 		Code int
 	}{Code: 200}
 	this.ServeJson()
+}
+
+func (this *UsersController) Post() {
+	beego.Debug("HELLO")
+	req := this.Ctx.Request
+
+	p := make([]byte, req.ContentLength)
+	user := new(userModel.User)
+	_, err := this.Ctx.Request.Body.Read(p)
+	if err == nil {
+		json.Unmarshal(p, &user)
+	}
+	//o := orm.NewOrm()
+
+	//json.Unmarshal(this.Ctx.Request.Body, &user)
+	//beego.Debug(this.Ctx.Input.Params)
+	this.Data["json"] = user
+	this.ServeJson()
+
 }
