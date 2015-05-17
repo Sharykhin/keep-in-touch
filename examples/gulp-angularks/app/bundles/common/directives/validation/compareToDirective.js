1428da0f6b95d306c2b141e045ec4b099c6f2267
Sharykhin <siarhei.sharykhin@gmail.com>
2015-05-17 21:56:40 +0300

    ISSUE-#34. Last directive was added

---
+++ examples/gulp-angularks/app/bundles/common/directives/validation/compareToDirective.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+var compareTo = function() {
+    return {
+	    require: 'ngModel',
+	    scope: {
+			otherModelValue: '=compareTo'
+		},
+		link: function(scope, element, attrs, ngModel) {
+				ngModel.$validators.compareTo = function(modelValue) {
+					return modelValue == scope.otherModelValue;
+				};
+
+				scope.$watch('otherModelValue', function() {
+					ngModel.$validate();
+				});
+			}
+	};
+};
+
+module.exports = compareTo;
--- examples/gulp-angularks/app/bundles/common/index.js
+++ examples/gulp-angularks/app/bundles/common/index.js
@@ -3,4 +3,5 @@
 var name = 'common';  
 var module = angular.module(name,[]);
 module.factory('ValidationService', require('./services/ValidationService'))
-	.directive('progressBar',require('./directives/progress/progressBarDirective'));
+	.directive('progressBar',require('./directives/progress/progressBarDirective'))
+	.directive('compareTo',require('./directives/validation/compareToDirective'));
